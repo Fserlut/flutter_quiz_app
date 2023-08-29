4c2b83f13d2c42d301caff90354ab8012be7d337
Ilya Zakharov <zakharovvilya@yandex.ru>
2023-08-30 02:04:09 +0400

    two screens

---
+++ lib/quiz.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:second_app/questions_screen.dart';
+
+import 'package:second_app/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  Widget? activeScreen;
+
+  @override
+  void initState() {
+    activeScreen = StartScreen(changeScreen);
+    super.initState();
+  }
+
+  void changeScreen() {
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 104, 20, 182),
+                Color.fromARGB(255, 87, 20, 204),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: activeScreen,
+        ),
+      ),
+    );
+  }
+}
